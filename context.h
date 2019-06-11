245aba648850e55af831b5ebd2f5e3e2f3d3752e
Vanja Buturac <vbuturac@racunarstvo.hr>
2019-06-11 09:46:29 +0200

    undo added

---
+++ context.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <emscripten.h>
+#include <emscripten/html5.h>
+#include <vector>
+using namespace std;
+
+
+
+class context
+{
+public:
+        
+
+    const unsigned int width = 600;
+    const unsigned int height = 400;
+    int x = 0;
+    int y = 0;
+    int xrel = 0;
+    int yrel = 0;
+    vector<vector<array<int,2>>> points;
+
+    
+
+    int drawing = 0;
+    SDL_Renderer *renderer;
+    /**
+     * Rectangle that the owl texture will be rendered into
+     */
+    SDL_Rect dest;
+    SDL_Texture *owl_tex;
+    /**
+     * Font that is used for rendering text, and
+     * a texture the text is rendered into
+     */
+    TTF_Font *font;
+    SDL_Texture *text_tex;
+    /**
+     * x and y components of owl's velocity
+     */
+    int owl_vx;
+    int owl_vy;
+
+};
--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
-#include <stdio.h>
-// #include "SDL2/SDL.h"
-// #include "SDL2/SDL_image.h"
-// #include "SDL2/SDL_ttf.h"
+
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
 #include <emscripten.h>
+#include <emscripten/html5.h>
+#include <emscripten/key_codes.h>
+// #include "SDL2/SDL.h"
+// #include "SDL2/SDL_image.h"
+// #include "SDL2/SDL_ttf.h"
 #include <iostream>
 #include <vector>
 #include <array>
+#include <string>
+#include "context.h"
 
 using namespace std;
 /**
@@ -16,57 +20,6 @@ using namespace std;
  */
 #define REC_SQRT2 0.7071067811865475 
 
-/**
- * Set of input states
- */
-enum input_state
-{
-    NOTHING_PRESSED = 0,
-    UP_PRESSED = 1,
-    DOWN_PRESSED = 1<<1,
-    LEFT_PRESSED = 1<<2,
-    RIGHT_PRESSED = 1<<3
-};
-
-/**
- * Context structure that will be passed to the loop handler
- */
-struct context
-{
-    const unsigned int width = 600;
-    const unsigned int height = 400;
-    int x = 0;
-    int y = 0;
-    int xrel = 0;
-    int yrel = 0;
-    vector<vector<array<int,2>>> points;
-
-    int drawing = 0;
-
-
-    SDL_Renderer *renderer;
-
-    /**
-     * Rectangle that the owl texture will be rendered into
-     */
-    SDL_Rect dest;
-    SDL_Texture *owl_tex;
-
-    /**
-     * Font that is used for rendering text, and
-     * a texture the text is rendered into
-     */
-    TTF_Font *font;
-    SDL_Texture *text_tex;
-
-    enum input_state active_state;
-
-    /**
-     * x and y components of owl's velocity
-     */
-    int owl_vx;
-    int owl_vy;
-};
 
 /**
  * Loads the owl texture into the context
@@ -116,8 +69,11 @@ int get_font_texture(context *ctx)
  */
 void process_input(context *ctx)
 {
-    SDL_Event event;
+    
 
+
+    SDL_Event event;
+    
 
     
     if (ctx->drawing==1){
@@ -126,105 +82,24 @@ void process_input(context *ctx)
         set_font_text(ctx, "NOT drawing");
     }
     while (SDL_PollEvent(&event)) {
+
+
     //cout << event.motion.x << endl
     //<< event.motion.y << endl;
         if (event.button.type == SDL_MOUSEBUTTONDOWN){
             if (event.button.button == SDL_BUTTON_LEFT){
-                ctx->drawing = 1;
-                vector<array<int,2>> temp;
-                temp.push_back({event.motion.x,event.motion.y});
-                ctx->points.push_back(temp);
+                //ctx->drawing = 1;
+                //vector<array<int,2>> temp;
+                //temp.push_back({event.motion.x,event.motion.y});
+                //ctx->points.push_back(temp);
             }
         }else if (event.button.type == SDL_MOUSEBUTTONUP){
             if (event.button.button == SDL_BUTTON_LEFT){
-                ctx->drawing = 0;
+                //ctx->drawing = 0;
             }
         }
-        
-        if (event.motion.type == SDL_MOUSEMOTION){
-            ctx->x=event.motion.x;
-            ctx->y=event.motion.y;
-            if (ctx->drawing){ 
-                ctx->points[ctx->points.size()-1].push_back({event.motion.x,event.motion.y});
-            }
-            ctx->xrel=event.motion.xrel;
-            ctx->xrel=event.motion.xrel;
-        }
 
-        
-        
-
-
-        
-        //cout << "state: " << (int)event.button.state << endl;
-        //cout << "button: " << (int)event.button.button << endl;
-        
-
-
-        // cout << event.motion.x << endl
-        //  << event.motion.xrel << endl
-        //  << event.motion.y << endl
-        //  << event.motion.yrel << endl;
     }
-
-
-
-    // int i = 2;
-    // //ctx->active_state = static_cast<input_state>(i);
-    // ctx->active_state = input_state(ctx->active_state | UP_PRESSED);
-    // //ctx->active_state |= static_cast<input_state>(UP_PRESSED);
-
-    // std::cout << ctx->active_state << std::endl;
-
-    // while (SDL_PollEvent(&event)) {
-    //     switch (event.key.keysym.sym)
-    //     {
-    //         case SDLK_UP:
-    //             if (event.key.type == SDL_KEYDOWN)
-    //                 ctx->active_state |= UP_PRESSED;
-    //             else if (event.key.type == SDL_KEYUP)
-    //                 ctx->active_state ^= UP_PRESSED;
-    //             break;
-    //         case SDLK_DOWN:
-    //             if (event.key.type == SDL_KEYDOWN)
-    //                 ctx->active_state |= DOWN_PRESSED;
-    //             else if (event.key.type == SDL_KEYUP)
-    //                 ctx->active_state ^= DOWN_PRESSED;
-    //             break;
-    //         case SDLK_LEFT:
-    //             if (event.key.type == SDL_KEYDOWN)
-    //                 ctx->active_state |= LEFT_PRESSED;
-    //             else if (event.key.type == SDL_KEYUP)
-    //                 ctx->active_state ^= LEFT_PRESSED;
-    //             break;
-    //         case SDLK_RIGHT:
-    //             if (event.key.type == SDL_KEYDOWN)
-    //                 ctx->active_state |= RIGHT_PRESSED;
-    //             else if (event.key.type == SDL_KEYUP)
-    //                 ctx->active_state ^= RIGHT_PRESSED;
-    //             break;
-    //         default:
-    //             break;
-    //     }
-    // }
-    // // ETC...
-    // ctx->owl_vy = 0;
-    // ctx->owl_vx = 0;
-    // if (ctx->active_state & UP_PRESSED)
-    //     ctx->owl_vy = -5;
-    // if (ctx->active_state & DOWN_PRESSED)
-    //     ctx->owl_vy = 5;
-    // if (ctx->active_state & LEFT_PRESSED)
-    //     ctx->owl_vx = -5;
-    // if (ctx->active_state & RIGHT_PRESSED)
-    //     ctx->owl_vx = 5;
-
-    // if (ctx->owl_vx != 0 && ctx->owl_vy != 0)
-    // {
-    //     ctx->owl_vx *= REC_SQRT2;
-    //     ctx->owl_vy *= REC_SQRT2;
-    // }
-
 
 }
 
@@ -276,6 +151,116 @@ void loop_handler(void *arg)
     SDL_RenderPresent(ctx->renderer);
 }
 
+int interpret_charcode_for_keyevent(int eventType, const EmscriptenKeyboardEvent *e)
+{
+  // Only KeyPress events carry a charCode. For KeyDown and KeyUp events, these don't seem to be present yet, until later when the KeyDown
+  // is transformed to KeyPress. Sometimes it can be useful to already at KeyDown time to know what the charCode of the resulting
+  // KeyPress will be. The following attempts to do this:
+  if (eventType == EMSCRIPTEN_EVENT_KEYPRESS && e->which) return e->which;
+  if (e->charCode) return e->charCode;
+  if (strlen(e->key) == 1) return (int)e->key[0];
+  if (e->which) return e->which;
+  return e->keyCode;
+}
+
+
+static inline const char *emscripten_event_type_to_string(int eventType) {
+    const char *events[] = { "(invalid)", "(none)", "keypress", "keydown", "keyup", "click", "mousedown", "mouseup", "dblclick", "mousemove", "wheel", "resize", 
+        "scroll", "blur", "focus", "focusin", "focusout", "deviceorientation", "devicemotion", "orientationchange", "fullscreenchange", "pointerlockchange", 
+        "visibilitychange", "touchstart", "touchend", "touchmove", "touchcancel", "gamepadconnected", "gamepaddisconnected", "beforeunload", 
+        "batterychargingchange", "batterylevelchange", "webglcontextlost", "webglcontextrestored", "(invalid)" };
+    ++eventType;
+    if (eventType < 0) eventType = 0;
+    if (eventType >= sizeof(events)/sizeof(events[0])) eventType = sizeof(events)/sizeof(events[0])-1;
+    return events[eventType];
+    }
+
+    int number_of_characters_in_utf8_string(const char *str)
+{
+  if (!str) return 0;
+  int num_chars = 0;
+  while(*str)
+  {
+    if ((*str++ & 0xC0) != 0x80) ++num_chars; // Skip all continuation bytes
+  }
+  return num_chars;
+}
+
+int emscripten_key_event_is_printable_character(const EmscriptenKeyboardEvent *keyEvent)
+{
+  // Not sure if this is correct, but heuristically looks good. Improvements on corner cases welcome.
+  return number_of_characters_in_utf8_string(keyEvent->key) == 1;
+}
+
+EM_BOOL key_callback(int eventType, const EmscriptenKeyboardEvent *e, void *userData)
+{
+    context* ctx = static_cast<context*>(userData);
+  int dom_pk_code = emscripten_compute_dom_pk_code(e->code);
+
+  printf("%s, key: \"%s\" %d (printable: %s), code: \"%s\" = %s (%d), location: %lu,%s%s%s%s repeat: %d, locale: \"%s\", char: \"%s\", charCode: %lu (interpreted: %d), keyCode: %s(%lu), which: %lu\n",
+    emscripten_event_type_to_string(eventType), e->key, eventType, emscripten_key_event_is_printable_character(e) ? "true" : "false", e->code,
+    emscripten_dom_pk_code_to_string(dom_pk_code), dom_pk_code, e->location,
+    e->ctrlKey ? " CTRL" : "", e->shiftKey ? " SHIFT" : "", e->altKey ? " ALT" : "", e->metaKey ? " META" : "", 
+    e->repeat, e->locale, e->charValue, e->charCode, interpret_charcode_for_keyevent(eventType, e), emscripten_dom_vk_to_string(e->keyCode), e->keyCode, e->which);
+
+  if (eventType == EMSCRIPTEN_EVENT_KEYUP) printf("\n"); // Visual cue
+    if(eventType==2 && e->ctrlKey && !e->repeat && e->which==90){
+        if (ctx->points.size())
+        {
+            ctx->drawing=0;
+            ctx->points.pop_back();
+        }
+        
+        
+    }
+  // Return true for events we want to suppress default web browser handling for.
+  // For testing purposes, want to return false here on most KeyDown messages so that they get transformed to KeyPress messages.
+  return e->keyCode == DOM_VK_BACK_SPACE // Don't navigate away from this test page on backspace.
+    || e->keyCode == DOM_VK_TAB // Don't change keyboard focus to different browser UI elements while testing.
+    || (e->keyCode >= DOM_VK_F1 && e->keyCode <= DOM_VK_F24) // Don't F5 refresh the test page to reload.
+    || e->ctrlKey // Don't trigger e.g. Ctrl-B to open bookmarks
+    || e->altKey // Don't trigger any alt-X based shortcuts either (Alt-F4 is not overrideable though)
+    || eventType == EMSCRIPTEN_EVENT_KEYPRESS || eventType == EMSCRIPTEN_EVENT_KEYUP; // Don't perform any default actions on these.
+}
+EM_BOOL mouse_callback(int eventType, const EmscriptenMouseEvent *e, void *userData)
+    {
+    context* ctx = static_cast<context*>(userData);
+
+    printf("%s,%ld, screen: (%ld,%ld), client: (%ld,%ld),%s%s%s%s button: %hu, buttons: %hu, movement: (%ld,%ld), canvas: (%ld,%ld), target: (%ld, %ld)\n",
+        emscripten_event_type_to_string(eventType), (long)eventType, e->screenX, e->screenY, e->clientX, e->clientY,
+        e->ctrlKey ? " CTRL" : "", e->shiftKey ? " SHIFT" : "", e->altKey ? " ALT" : "", e->metaKey ? " META" : "", 
+        e->button, e->buttons, e->movementX, e->movementY, e->canvasX, e->canvasY, e->targetX, e->targetY);
+
+        if(e->buttons==1){
+            ctx->drawing=1;
+            
+        }else{
+            ctx->drawing=0;
+        }
+
+        switch(eventType){
+            case 5:
+                if (ctx->drawing){
+                    //vector<array<int,2>> temp;
+                    //temp.push_back({e->canvasX,e->canvasY});
+                    //ctx->points.push_back(temp);
+                    ctx->points.push_back({{e->canvasX,e->canvasY}});
+
+                    //ctx->points.pushback(vector<array<int,2>>());
+                }
+                break;
+            case 8:
+                if (ctx->drawing){
+                    ctx->points[ctx->points.size()-1].push_back({e->canvasX,e->canvasY});
+                }
+                break;
+        }
+
+
+    return 0;
+
+    }
+
 int main()
 {
     SDL_Window *window;
@@ -289,16 +274,22 @@ int main()
 
     get_owl_texture(&ctx);
     get_font_texture(&ctx);
-    ctx.active_state = NOTHING_PRESSED;
     ctx.dest.x = 200;
     ctx.dest.y = 100;
     ctx.owl_vx = 0;
     ctx.owl_vy = 0;
+    
+    emscripten_set_click_callback(0, &ctx, 1, mouse_callback);
+    emscripten_set_mousedown_callback(0, &ctx, 1, mouse_callback);
+    emscripten_set_mouseup_callback(0, &ctx, 1, mouse_callback);
+    emscripten_set_dblclick_callback(0, &ctx, 1, mouse_callback);
+    emscripten_set_mousemove_callback(0, &ctx, 1, mouse_callback);
+    emscripten_set_keydown_callback(0, &ctx, 1, key_callback);
+    emscripten_set_keyup_callback(0, &ctx, 1, key_callback);
+    emscripten_set_keypress_callback(0, &ctx, 1, key_callback);
 
-    /**
-     * Schedule the main loop handler to get 
-     * called on each animation frame
-     */
+    //ret = emscripten_set_wheel_callback(0, 0, 1, wheel_callback);
+
     emscripten_set_main_loop_arg(loop_handler, &ctx, -1, 1);
 
     return 0;
